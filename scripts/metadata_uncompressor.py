c54c9b3f61a75af363d2feb3b129095c5a5ddda7
Floris <49408883+EV4g@users.noreply.github.com>
2025-11-14 14:08:23 +0100

    Add metadata uncompressor script

---
+++ scripts/metadata_uncompressor.py
@@ -0,0 +1,22 @@
+import os
+import glob
+from pathlib import Path
+import subprocess
+
+filelocation = "linc_out/L*/results/*ms"
+outdir = "linc_out_uncompressed/"
+
+filelist = glob.glob(filelocation)
+
+for i, msfile in enumerate(filelist):
+    observation = Path(msfile).parts[1]
+    filename = Path(msfile).parts[-1]
+
+    print("["+str(i + 1)+" / "+str(len(filelist))+"]", filename)
+
+    outlocation = os.path.join(outdir, observation, "results")
+    os.makedirs(outlocation, exist_ok=True)
+    outfile = os.path.join(outlocation, filename)
+
+    command = f"DP3 msin={msfile} msout={outfile} steps=[] msout.scalarflags=False  msout.uvw>
+    subprocess.run(command, shell=True, check=True, executable="/bin/bash")
