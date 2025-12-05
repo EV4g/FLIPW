2765869a454f2e9ba166ca462abf882e424766e8
Floris <49408883+EV4g@users.noreply.github.com>
2025-12-05 14:56:25 +0100

    Refactor generate_RAPTHOR_parset for multiple observations

--- scripts/RAPTHOR/generate_RAPTHOR_parset.py
+++ scripts/RAPTHOR/generate_RAPTHOR_parset.py
@@ -1,4 +1,6 @@
+import numpy as np
 import os
+import glob
 import subprocess
 import argparse
 from pathlib import Path
@@ -11,7 +13,7 @@ except:
 
 # ensure observation ID is passed
 parser = argparse.ArgumentParser(description='Setting up rapthor parset')
-parser.add_argument('--observation', type=str, help='observation-ID')
+parser.add_argument('--observation', type=str, help='observation-ID', default="all")
 args = parser.parse_args()
 
 observation = args.observation
@@ -26,35 +28,60 @@ else:
     print(colored("defaults.parset downloaded", "green"))
 
 # check for linc_out directories
-if os.path.isdir(os.path.join("linc_out_uncompressed", observation)):
-    data_path = os.path.abspath(os.path.join("linc_out_uncompressed", observation))
-    print(colored("Found linc_out_uncompressed", "green"), "at", data_path)
-elif os.path.isdir(os.path.join("linc_out", observation)):
-    data_path = os.path.abspath(os.path.join("linc_out", observation))
-    print(colored("Found linc_out", "green"), "at", data_path)
+if os.path.isdir(os.path.join("linc_out_uncompressed")):
+	linc_dir = os.path.abspath(os.path.join("linc_out_uncompressed"))
+    print(colored("Found linc_out_uncompressed", "green"), "at", linc_dir)
+elif os.path.isdir(os.path.join("linc_out")):
+	linc_dir = os.path.abspath(os.path.join("linc_out"))
+    print(colored("Found linc_out", "green"), "at", linc_dir)
 else:
-	print(colored("No linc_out folder found", "yellow"))
+    print(colored("No linc_out folder found", "yellow"))
+
+
+# check if observation is given, if not, check all files in linc_dir and repeat for all observations present
+# put observation in list, such that it is iterable
+if observation == 'all':
+    observation = np.sort([folder.split("/")[-1] for folder in glob.glob(linc_dir + "/*")])
+else:
+    observation = [observation]
+
+print(colored(f"Found {len(observation)} observations:", "green"), observation, "\n")
 
 # make separate RAPTHOR output directory
-rapthor_out_dir = "rapthor_out_"+observation
-os.makedirs(rapthor_out_dir, exist_ok=True)
+data_paths = []
+for obs in observation:
+    data_paths.append(os.path.join(linc_dir, obs))
+
+rapthor_out_dirs = []
+for obs in observation:
+    rapthor_dir = os.path.join(os.getcwd(),"rapthor_out_"+obs)
+    os.makedirs(rapthor_dir, exist_ok=True)
+    print(colored("Made dir:", "green"), rapthor_dir)
+    rapthor_out_dirs.append(rapthor_dir)
+print("")
 
 # then replace some of the lines in there, as opposed to writing it in this function
 in_file = Path("defaults.parset")
-out_file = Path(observation+".parset")
 
 with in_file.open("r", encoding="utf-8") as f: lines = f.readlines()
 
-new_lines = []
-for line in lines:
-    # maybe easier to hardcode linenumbers, but maybe the file will eventually change
-    if line.startswith("dir_working ="): line = "dir_working ="+rapthor_out_dir+"\n"
-    if line.startswith("input_ms ="): line = "input_ms ="+data_path+"/*.ms\n"
+for i, obs in enumerate(observation):
+    out_file = Path(os.path.join(rapthor_out_dirs[i], observation[i]+".parset"))
 
-    # optional line that can be useful if the compression fails
-    if line.startswith("compress_selfcal_images ="): line = "compress_selfcal_images = False"
+    new_lines = []
+    for line in lines:
+        # maybe easier to hardcode linenumbers, but maybe the file will eventually change
+        if line.startswith("dir_working ="): line = "dir_working = "+rapthor_out_dirs[i]+"\n"
+        if line.startswith("input_ms ="): line = "input_ms = "+data_paths[i]+"/*.ms\n"
 
-	#if line.startswith(""): line = ""
-    #if	line.startswith(""): line = ""
+        # optional line that can be useful if the compression fails
+        if line.startswith("compress_selfcal_images ="): line = "compress_selfcal_images = False"
 
-    new_lines.append(line)
+        #if line.startswith(""): line = ""
+        #if line.startswith(""): line = ""
+
+        new_lines.append(line)
+
+    with out_file.open("w", encoding="utf-8") as f:
+        f.writelines(new_lines)
+    print(colored("Written file", "green"), "to", out_file)
