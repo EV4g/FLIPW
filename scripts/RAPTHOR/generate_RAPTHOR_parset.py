cc9514059c7fa380d1c79a27907775ea9164d8dc
Floris <49408883+EV4g@users.noreply.github.com>
2025-12-08 13:20:35 +0100

    Update generate_RAPTHOR_parset.py

--- scripts/RAPTHOR/generate_RAPTHOR_parset.py
+++ scripts/RAPTHOR/generate_RAPTHOR_parset.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import os
 import glob
@@ -29,10 +30,10 @@ else:
 
 # check for linc_out directories
 if os.path.isdir(os.path.join("linc_out_uncompressed")):
-	linc_dir = os.path.abspath(os.path.join("linc_out_uncompressed"))
+    linc_dir = os.path.abspath(os.path.join("linc_out_uncompressed"))
     print(colored("Found linc_out_uncompressed", "green"), "at", linc_dir)
 elif os.path.isdir(os.path.join("linc_out")):
-	linc_dir = os.path.abspath(os.path.join("linc_out"))
+    linc_dir = os.path.abspath(os.path.join("linc_out"))
     print(colored("Found linc_out", "green"), "at", linc_dir)
 else:
     print(colored("No linc_out folder found", "yellow"))
@@ -52,12 +53,16 @@ data_paths = []
 for obs in observation:
     data_paths.append(os.path.join(linc_dir, obs))
 
+# check that folders exist to put the batch into
 rapthor_out_dirs = []
 for obs in observation:
     rapthor_dir = os.path.join(os.getcwd(),"rapthor_out_"+obs)
-    os.makedirs(rapthor_dir, exist_ok=True)
-    print(colored("Made dir:", "green"), rapthor_dir)
-    rapthor_out_dirs.append(rapthor_dir)
+    if not(os.path.isdir(rapthor_dir)):
+        os.makedirs(rapthor_dir, exist_ok=True)
+        print(colored("Made dir:", "green"), rapthor_dir)
+    else:
+        print(colored("Dir:", "green"), rapthor_dir, colored("already exists", "green"))
+        rapthor_out_dirs.append(rapthor_dir)
 print("")
 
 # then replace some of the lines in there, as opposed to writing it in this function
@@ -85,3 +90,31 @@ for i, obs in enumerate(observation):
     with out_file.open("w", encoding="utf-8") as f:
         f.writelines(new_lines)
     print(colored("Written file", "green"), "to", out_file)
+print("")
+
+
+def write_slurm_script(observation, linc_dir):
+    script_content = f"""#!/bin/bash
+#SBATCH --job-name=RAP{observation}
+#SBATCH --output=slurm_%j_{observation}.out
+#SBATCH -N 1
+#SBATCH -c 60
+#SBATCH -t 120:00:00
+
+cd /project/lspc/Data/floris/run-rapthor/rapthor_out_{observation}
+
+# Base folder
+binds="/project/lspc/Data/floris/run-rapthor/{linc_dir},/project/lspc/Data/floris/run-rapthor,/project/lspc/Data/floris/catalogues"
+
+singularity exec -B $binds -H /home/lspc-fmartens01/ ../rapthor_latest.sif rapthor {observation}.parset
+"""
+    file_path = os.path.join(f"rapthor_out_{observation}", f"{observation}_rapthor_batch.sh")
+    with open(file_path, "w") as file:
+        file.write(script_content)
+
+# make .sh scripts for all observations
+for obs in observation:
+    write_slurm_script(obs, linc_dir)
+    print(colored("Made file:", "green"), f"{obs}_rapthor_batch.sh")
+
+
