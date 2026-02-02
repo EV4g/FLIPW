7fb11fb5a2638200a3ffe05c153485ba621c7bc7
Floris <49408883+EV4g@users.noreply.github.com>
2026-02-02 19:29:05 +0100

    Create setup_new_test.py

---
+++ scripts/DDF/setup_new_test.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+import argparse
+import glob
+
+parser = argparse.ArgumentParser(description='Setting up new imaging test')
+parser.add_argument('--name', type=str, help="Name of new test", required=True)
+
+args = parser.parse_args()
+
+# copy template folder to new location
+#os.system(f"cp -r template_folder {args.name}")
+
+# should run "setup_new_test.py --name=..."
+linc_dir = os.path.join(args.name, "linc_out_uncompressed/*")
+observation = glob.glob(linc_dir)[0].split("/")[-1].split("_")[0]
+
+subprocess.run(["python3", "make_gal_ddf_batch.py",
+f"--workdir={args.name}",
+f"--linc_dir={linc_dir}",
+f"--output_name={args.name}",
+f"--mask=test_mask.fits"])
+
+
+
+# update big_mslist.txt
+# update batch_SSD2.sh
